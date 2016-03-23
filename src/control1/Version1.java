b923912c4e75c2158611f46282921886aafc0b82
xavier.joseph <xavier.joseph@D402.blr.velankani.com>
2016-03-23 12:39:01 +0530

    version7

--- src/control1/Version1.java
+++ src/control1/Version1.java
@@ -13,5 +13,6 @@ public class Version1
 	 System.out.println("version6");
 	 System.out.println("localbranch1");
 	 System.out.println("localbranch1-commit2");
+	 System.out.println("version7");
  }
 }
